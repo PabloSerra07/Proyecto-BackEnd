80948ea80da6a9b422f923946fcec0d36382ace6
PabloSerra07 <pablojaviej@gmail,com>
2022-11-15 23:02:30 -0300

    first commit

---
+++ prueba.js
@@ -0,0 +1,37 @@
+// let nombre = "pepe";
+// let edad = 30;
+// let precio = 100;
+
+// const seriesFavoritas = [`Futurama`,  `Los Simpson`,` nose` ];
+
+// const peliculasFavoritas = {peli1: `aaaa`, peli2: `bbbb`, peli2: `ccc`};
+
+// console.log('nombre: ', nombre);
+// console.log('edad: ', edad);
+// console.log('precio: ', precio);
+// console.log('seriesFavoritas: ', seriesFavoritas);
+// console.log('moviesFavoritas: ', peliculasFavoritas);
+
+
+// edad++;
+// console.log('edad aumentada: ', edad);
+
+// seriesFavoritas.push('Futurama');
+// console.log('nuevo array seriesFavoritas : ', seriesFavoritas);
+
+
+// class Contador {
+//     constructor (nombre, apellido, cuenta ){
+//         this.nombre = nombre
+//         this.apellido = apellido
+//         this.cuenta = cuenta
+//     }   
+// }
+// const asa = new Contador (`pepe`, `grillo` ,0)
+// const asa1 = new Contador (`nose`, `algo` ,0)
+// const asa2 = new Contador (`capas`, `puedeser` ,0)
+
+// console.log(asa)
+// console.log(asa1)
+// console.log(asa2)
+
