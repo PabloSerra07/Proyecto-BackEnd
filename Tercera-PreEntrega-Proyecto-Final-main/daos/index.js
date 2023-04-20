a5b0a4584ca35f5ab382f8e7180c3a95f0d63cde
PabloSerra07 <pablojaviej@gmail,com>
2023-04-20 09:52:37 -0300

    3er-entrega-final

--- Tercera-PreEntrega-Proyecto-Final-main/daos/index.js
+++
@@ -1,5 +0,0 @@
-import CartDaoMongoDB from "./carts/cartDaoMongoDB.js";
-import ProductsDaoMongoDB from "./products/productsDaoMongoDB.js";
-
-export const cartDaoMongoDB = new CartDaoMongoDB();
-export const productsDaoMongoDB = new ProductsDaoMongoDB();
